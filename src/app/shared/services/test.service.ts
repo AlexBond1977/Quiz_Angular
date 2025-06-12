81e141ce55ae0aed7573f8e183e03cc4fff6fcae
Alexey Bondarev <6933666@mail.ru>
2025-06-12 17:08:35 +0300

    Реализация функционала прохождения теста и вывода результата

--- src/app/shared/services/test.service.ts
+++ src/app/shared/services/test.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {QuizListType} from "../../../types/quiz-list.type";
 import {DefaultResponseType} from "../../../types/default-response.type";
 import {TestResultType} from "../../../types/test-result.type";
+import {QuizType} from "../../../types/quiz.type";
+import {UserResultType} from "../../../types/user-result.type";
+import {PassTestResponseType} from "../../../types/pass-test-response.type";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   // сервис отправки запросов для получения тестов
   getTests(): Observable<QuizListType[]> {
@@ -22,5 +26,26 @@ export class TestService {
   getUserResults(userId: number): Observable<DefaultResponseType | TestResultType[]> {
     return this.http.get<DefaultResponseType | TestResultType[]>(environment.apiHost + 'tests/results?userId=' + userId);
   }
+
+  // сервис для получения тестов
+  getQuiz(id: number | string): Observable<DefaultResponseType | QuizType> {
+    return this.http.get<DefaultResponseType | QuizType>(environment.apiHost + 'tests/' + id);
+  }
+
+  // сервис для получения результатов тестов
+  passQuiz(id: number | string, userId: number | string, userResult: UserResultType[]): Observable<DefaultResponseType | PassTestResponseType> {
+    return this.http.post<DefaultResponseType | PassTestResponseType>(environment.apiHost + 'tests/' + id + '/pass',
+      {
+        userId: userId,
+        results: userResult
+      });
+  }
+
+
+  // сервис для получения результата теста
+  getResult(id: number | string, userId: number | string): Observable<DefaultResponseType | PassTestResponseType> {
+    return this.http.get<DefaultResponseType | PassTestResponseType>(environment.apiHost + 'tests/' + id + '/result?userId=' + userId);
+  }
+
 
 }
